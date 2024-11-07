cd78fa3b6cc71d3808262cbbb56d543acee1d1f4
dorio <@BriceAstrion>
2024-11-07 16:00:17 +0100

    Initialised users data

---
+++ template/server/src/data/users.js
@@ -0,0 +1,58 @@
+export default [
+    {
+        id: 1,
+        name: "brice",
+        isAdmin: true,
+        email:"brice@gmail.com",
+        password: "$2b$10$.cinO1texHt4CFeY/ZWMre5hHVgxQEJ9udSlOQY5dRLsfmPvW9SLS", //pw: Mö08br2324
+    },
+    {
+        id: 2,
+        name:"gerrald",
+        isAdmin: false,
+        email: "gerrald@gmail.com",
+        password: "$2y$10$7ScV1Ap.1XJnF.8REKBY4eyXRTU5jrIBNtDhRbqQgLIr2t3rMn3qy", //pw: Reshbh129
+    },
+    {
+        id: 3,
+        name:"ange",
+        isAdmin: false,
+        email: "ange@gmail.com",
+        password: "$2b$10$REVz82XRrGv7nxuRikiSGuLUt227Aypq5LD/qBhOOL1ypESksiAZW", //pw: Rå15vu2324
+    },
+    {
+        id: 4,
+        email: "astrion@gmail.com",
+        password: "$2a$12$2y5EnDJQHhlA8k.4ULRp4OXQNbre.YaXY51T7/z5hg0QjFueKrX5q", //pw: Welcome123
+        isAdmin: true,
+        name: "astrion"
+    },
+    {
+        id: 5,
+        email: "gabrielle@gmail.com",
+        password: "$2y$10$/LQs4.PeBfQoPVjzlNGZ0OzF4KTE.eQokkouXbR.2AZcrHILAEgfW", //pw: RFhb242
+        isAdmin: false,
+        name: "gabrielle"
+    },
+    {
+        id: 6,
+        email: "chloes@gmail.com",
+        password: "$2a$12$X0QyuAufcIONyNhbdCnMA.NEnHekEfzQueX.9lex3/KzMy8XlA1DS", //pw: Esta237
+        isAdmin: false,
+        name: "iris"
+    },
+    {
+        id: 7,
+        name: "ronald",
+        isAdmin: false,
+        email:"ronald@gmail.com",
+        password: "$2a$12$8IK8CiJd/.F4ai63Rf.Ciu5.c//1eQkAfL4nOFr28sEp8VprDeGM6", //pw: NLjawel202425
+    },
+    {
+        id: 8,
+        name: "carina",
+        isAdmin: false,
+        email:"carina@gmail.com",
+        password: "$2a$12$0CBavwtd8lcVnKfO72CFpu/BSiHxWyLIAdfCdwL5giV90Jj9g/VXu", //pw: Proud55
+    },
+];

5fe689bb9c5b93f3e0b7fade5c8b99f111a49a06
dorio <@BriceAstrion>
2024-11-07 17:16:03 +0100

    Added searchBar and Filter components

--- template/client/src/pages/Home.svelte
+++ template/client/src/pages/Home.svelte
@@ -1,7 +1,62 @@
 <script>
-    import Counter from '../lib/Counter.svelte';
+    import SearchBar from '../components/SearchBar.svelte';
+    import Filters from '../components/Filter.svelte';
+    import { ArtworksFiltered } from "../stores/auth.js";
 
-    export let params;
 </script>
 
-<Counter />
+<div class="main-container">
+    <Filters />
+</div>
+
+<main class="home">
+    <SearchBar />
+
+    <div class="artwork-listing">
+        {#each $ArtworksFiltered as item}
+            <div class="artwork">
+                <img src={item.imageUrl} alt={item.title} />
+                <h3>{item.title}</h3>
+            </div>
+        {/each}
+    </div>
+
+</main>
+
+<style>
+    .main-container {
+        display: flex;
+        padding: 20px;
+    }
+
+    .content {
+        flex-grow: 1;
+        margin-left: 20px;
+    }
+
+    .artwork-listing {
+        display: flex;
+        flex-wrap: wrap;
+        gap: 20px;
+        justify-content: space-between;
+        margin-top: 20px;
+    }
+
+    .artwork {
+        width: calc(33.33% - 20px);
+        padding: 10px;
+        box-sizing: border-box;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+        background-color: #f8efe4;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        text-align: center;
+    }
+
+    .artwork img {
+        max-width: 100%;
+        height: auto;
+    }
+</style>

91fa652bc767b38de9dbec077abfd6cd69a1f85b
dorio <@BriceAstrion>
2024-11-07 16:27:42 +0100

    set up users and token router

--- template/server/src/index.js
+++ template/server/src/index.js
@@ -1,17 +1,23 @@
 import express from 'express';
+import cors from 'cors';
+import users from './routes/users.js';
+import tokensRouter from './routes/tokens.js';
+import bodyParser from "body-parser";
+
 const app = express()
 const port = 3000
 
-import auth from './routes/auth.js';
+app.use(express.json());
 
-app.use("/auth", auth);
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  console.log(req);
-  // res.send('Hello World!')
-  res.json({ msg: "hello world"});
-})
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors ({ origin: 'http://localhost:5173' }));
+
+app.use("/users", users);
+app.use("/tokens", tokensRouter)
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Website app listening on port ${port}`)
 })

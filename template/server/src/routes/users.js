0981110693060101bbd7bac078a9e402475fffa7
dorio <@BriceAstrion>
2024-11-07 16:20:16 +0100

    users api call

---
+++ template/server/src/routes/users.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import UserController from '../controllers/users.js';
+import {isAdmin} from "../middlewares/isAdmin.js";
+import {isLoggedIn} from "../middlewares/isLoggedIn.js";
+
+const router = express.Router();
+const userController = new UserController();
+
+router.get('/', isLoggedIn, isAdmin, userController.getAllUsers);
+router.get('/id/:id', isLoggedIn, isAdmin, userController.getUserById);
+router.get('/:email', (req, res) => userController.getOneUser(req, res));
+
+router.post('/', (req, res) => userController.registerUser(req, res));
+
+router.put('/:email', isLoggedIn, isAdmin, userController.updateUser);
+
+router.delete('/:email',isLoggedIn, isAdmin, (req, res) => userController.deleteUser(req, res));
+router.delete('/:id',isLoggedIn, isAdmin, (req, res) => userController.deleteUserById(req, res));
+
+export default router;

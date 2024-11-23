e9e766a5e57273ba0f9382656d8ecea0caedc46c
dorio <@BriceAstrion>
2024-11-23 15:07:12 +0100

    artworks data

---
+++ template/server/src/data/artworks.js
@@ -0,0 +1,72 @@
+let items = [
+    {
+        id: 1,
+        imgSrc: "https://render.fineartamerica.com/images/images-profile-flow/400/images-medium-large-5/love-under-the-coconut-trees-emmanuel-baliyanga.jpg",
+        title : "Love Under The Coconut Trees",
+        artist: "Emmanuel Baliyanga",
+        yearOfRelease: 1999,
+        originalPrice: 899.55,
+        category: "Painting",
+        country: "Cameroon",
+        startDate: "2024-10-01 12:00 AM",
+        endDate: "2024-12-22 11:59 PM",
+        bids: [
+            {
+                bidId: 1,
+                user: " brice@gmail.com",
+                amount: 1798,
+                date: "2024-10-09 01:34 AM"
+            }
+        ]
+    },
+
+    {
+        id: 2,
+        imgSrc: "https://www.singulart.com/images-sh/eyJidWNrZXQiOiJzaW5ndWxhcnQtd2Vic2l0ZS1wcm9kIiwia2V5IjoiYXJ0d29ya3NcL3YyXC9jcm9wcGVkXC8xMjk2OVwvbWFpblwvY2Fyb3VzZWxcLzQzNzExM19iZjFkOTI2MzBjYTYxMjgzODdmNmU0ZGQyNmNhZmQzYy5qcGVnIiwiZWRpdHMiOnsicmVzaXplIjp7IndpZHRoIjo0OTgsImhlaWdodCI6MzI4LCJmaXQiOiJpbnNpZGUiLCJiYWNrZ3JvdW5kIjpudWxsfSwidG9Gb3JtYXQiOiJ3ZWJwIn19?signature=faa4c96133bb021089119deed7a3cc8f63e09d4641dd7cd0347423e26e2fe16c",
+        title: "The World",
+        artist: "Isabelle Renou",
+        yearOfRelease: 2005,
+        originalPrice: 12160,
+        category: "Sculpture",
+        country: "Morocco",
+        startDate: "2024-10-02 12:30 AM",
+        endDate: "2024-12-22 11:59 PM",
+        bids: [
+            {
+                bidId: 2,
+                user: "gerrald@gmail.com",
+                amount: 13100,
+                date: "2024-10-09 11:34 AM"
+            },
+            {
+                bidId: 3,
+                user: "ange@gmail.com",
+                amount: 14540,
+                date: "2024-10-09 13:52 PM"
+            }
+        ]
+    },
+
+    {
+        id: 3,
+        imgSrc: "https://render.fineartamerica.com/images/images-profile-flow/400/images/artworkimages/mediumlarge/1/5-the-equatorial-jungle-henri-rousseau.jpg",
+        title: "The Equatorial Jungle",
+        artist: "Henri Rousseau",
+        yearOfRelease: 2016,
+        originalPrice: 740,
+        category: "Painting",
+        country: "Cameroon",
+        startDate: "2024-09-12 12:00 AM",
+        endDate: "2025-08-12 10:00 PM",
+        bid: [
+            {
+                id: 4,
+                user: "gabrielle@gmail.com",
+                amount: 850,
+                date: "2024-10-10 10:25 PM"
+            }
+        ]
+    }
+]
+
+export default items;
